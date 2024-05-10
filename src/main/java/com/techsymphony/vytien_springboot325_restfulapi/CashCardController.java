5f21b2820738dee9e365dafe154e00d7a92d9564
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-05-10 14:51:57 +0700

    Remove unused package

--- src/main/java/com/techsymphony/vytien_springboot325_restfulapi/CashCardController.java
+++ src/main/java/com/techsymphony/vytien_springboot325_restfulapi/CashCardController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
-
 @RestController
 @RequestMapping(path = "/cashcards")
 public class CashCardController {
--- src/test/java/com/techsymphony/vytien_springboot325_restfulapi/VytienSpringboot325RestfulapiApplicationTests.java
+++ src/test/java/com/techsymphony/vytien_springboot325_restfulapi/VytienSpringboot325RestfulapiApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
